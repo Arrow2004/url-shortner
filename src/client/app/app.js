c9837ff668005c0d0e7dcbf44fafdb257d8985e3
@Arrow2004 <arrow2004hacker@gmail.com>
2021-07-10 22:20:49 +0500

    This is 3-commit

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -1,38 +1,45 @@
-const button = document.getElementById('submit');
-const results = document.getElementById('results')
-button.addEventListener('click', (e) =>{
-    const postData = async(postUrl, url = {}) => {
-        const res = await fetch(postUrl, {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(url),
-        })
-        try {
-            return await res.json()
-        } catch (error) {
-            console.log('error', error)
-        }
-         }
-    fullUrl = document.getElementById('urlInput').value;
-    console.log(fullUrl)
-    const response = postData('http://localhost:2000/fUrl', {
+const button = document.getElementById("submit");
+const results = document.getElementById("results");
+const clipboardB = document.getElementById("clipboardB");
+let url = "https://instagram.com/akbarcoder";
+button.addEventListener("click", async (e) => {
+  const postData = async (postUrl, url = {}) => {
+    const res = await fetch(postUrl, {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(url),
+    });
+    try {
+      return await res.json();
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+  fullUrl = document.getElementById("urlInput").value;
+  console.log(fullUrl);
+  let response = postData("http://localhost:2000/fUrl", {
     url: fullUrl,
-    })
-
-    response.then((res) => {
-        console.log('Response keldi');
-        console.log(res);
-        results.innerHTML = `You short url: <a class="shortUrl" href="${res.fU}" target="_blank">${res.fU}</a>`
-    })
-})
-function copyFunction() {
-    var copyText = document.getElementsByTagName("a");
-    var input = document.getElementById('clipboard')
-    input.value = copyText[0].innerHTML;
-    input.select();
-    document.execCommand('copy');
-    document.getElementById('clipboardB').innerHTML = 'Copied this url'
-  }
+  });
+  response = await response;
+  console.log(response.fU);
+  results.innerHTML = `You short url: <a class="shortUrl" href="${response.fU}" target="_blank">${response.fU}</a>`;
+  url = response.fU;
+});
+function copyToClipboard(text) {
+  var dummy = document.createElement("textarea");
+  document.body.appendChild(dummy);
+  dummy.value = text;
+  dummy.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummy);
+}
+clipboardB.addEventListener("click", (e) => {
+  copyToClipboard(url);
+  clipboardB.innerHTML = "Copied short url!!!";
+  setTimeout(() => {
+    clipboardB.innerHTML = "Copy short url!";
+  }, 3000);
+});
