ccff152e62b10fa202493703dd3f35b6f7b79f90
@Arrow2004 <arrow2004hacker@gmail.com>
2021-07-10 13:48:13 +0500

    This is 3-commit

--- src/server/server.js
+++ src/server/server.js
@@ -1,46 +1,96 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const randomId = require("random-id");
+
+const domain = "http://localhost:2000/";
 const app = express();
-const fs = require('fs')
-const cors = require('cors');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        bodyParser = require('body-parser')
-let urls = [[11111,"https://t99323py.beget.tech/"],[11112,"https://kun.uz/"],[11113,"https://youtube.com/"]];
+const dirN = __dirname.slice(0, __dirname.length - 7) + "/client";
+
+// Mongodbga ulanish
+mongoose
+  .connect(
+    "mongodb+srv://root:shirina1708@cluster0.ufftb.mongodb.net/url-shortner?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+// Model hamda sxema
+const urlScheme = new mongoose.Schema({
+  id: String,
+  shUrl: String,
+  oUrl: String,
+  date: { type: Date, default: Date.now },
+});
+const ShUrl = mongoose.model("Url", urlScheme);
+
 app.use(express.json());
 app.use(bodyParser());
-app.use(cors()); 
-dirN = __dirname.slice(0,__dirname.length-7)+'/client';
-app.use(express.static(dirN))
-app.listen(2000,()=>{
-    console.log(`Server is running on port: 2000`);
-    console.log(dirN)                                                                                                                                                                                                                                                                              
-})                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-app.get('/',(req,res)=>{                                                                                                                                                                                                            
-    console.log('Server is working...');
-    res.sendFile('index.html')
+app.use(cors());
+app.use(express.static(dirN));
+
+// Route parametrlar
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
 });
-app.post('/fUrl',(req,res)=>{
-    console.log('Habar keldi');
-    console.log(req.body)
-    len = urls.length;
-    idNumber = (urls[len-1][0]+1)
-    console.log(idNumber)
-    newUrl = [idNumber, req.body.url]
-    urls.push(newUrl)
-    shortUrl = {
-        done: true,
-        url: `?id=${idNumber}`,
-        host: 200,
-        fU: `http://localhost:2000/shUrl?id=${idNumber}`
-    }
-    setTimeout(() => {
-        res.send(shortUrl);
-    }, 1000);
-})
-app.get('/shUrl',(req,res)=>{
-    id = Number(req.query.id);
-    let fullUrl
+
+app.post("/fUrl", async (req, res) => {
+  const findUrl = await ShUrl.find({
+    oUrl: req.body.url,
+  });
+  let shortUrl = {};
+  if (findUrl[0] == undefined) {
+    const id = randomId(5, "a");
+    console.log(req.body);
     console.log(id);
-    if(urls[id-11111]!=undefined){
-        res.write('<script> window.open(\''+urls[id-11111][1]+'\',\"_parent\")</script>')
-    } else{
-        res.sendFile('not-found.html', {root: './src/client'})
-    }    
-})
+    shortUrl = {
+      done: true,
+      query: `?id=${id}`,
+      host: 200,
+      fU: `${domain}shUrl?id=${id}`,
+    };
+    const url = new ShUrl({
+      id: id,
+      shUrl: shortUrl.fU,
+      oUrl: req.body.url,
+    });
+    const saveUrl = await url.save();
+  } else {
+    console.log("Else boshlandi...");
+    shortUrl = {
+      done: true,
+      query: `?id=${findUrl[0].id}`,
+      host: 200,
+      fU: findUrl[0].shUrl,
+    };
+  }
+  setTimeout(() => {
+    res.send(shortUrl);
+  }, 1000);
+});
+
+app.get("/shUrl", async (req, res) => {
+  const url = await ShUrl.find({
+    id: req.query.id,
+  });
+  if (url[0] !== undefined) {
+    res.write(`<script> window.open('${url[0].oUrl}',"_parent")</script>`);
+  } else {
+    res.sendFile("not-found.html", { root: "./src/client" });
+  }
+});
+
+//Server
+app.listen(2000, () => {
+  console.log(`Server is running on port: 2000`);
+});
